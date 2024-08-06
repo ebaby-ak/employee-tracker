79049b1ede732c06c3cfa970efea7136928e1068
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-08-06 13:03:07 -0500

    added seeds for the database

--- db/schema.sql
+++ db/schema.sql
@@ -9,16 +9,16 @@ CREATE TABLE department (
     );
 
     CREATE TABLE role (
-       id: SERIAL PRIMARY KEY,
-       title: VARCHAR(30) UNIQUE NOT NULL,
-       salary: DECIMAL NOT NULL,
-       department_id: INTEGER NOT NULL
+       id SERIAL PRIMARY KEY,
+       title VARCHAR(30) UNIQUE NOT NULL,
+       salary DECIMAL NOT NULL,
+       department_id INTEGER NOT NULL
     );
 
     CREATE TABLE employee (
-        id: SERIAL PRIMARY KEY
-        first_name: VARCHAR(30) NOT NULL,
-        last_name: VARCHAR(30) NOT NULL,
-        role_id: INTEGER NOT NULL,
-        manager_id: INTEGER
+        id SERIAL PRIMARY KEY
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR(30) NOT NULL,
+        role_id INTEGER NOT NULL,
+        manager_id INTEGER
     );
