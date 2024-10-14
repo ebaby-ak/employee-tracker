db6446b0e5a45016014445ab366a05e9497a3813
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-10-13 19:43:42 -0500

    brought in starter code

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,35 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS hrTracker_db;
 
-\c employee_db
+-- Create the new database
+CREATE DATABASE hrTracker_db;
 
+-- Connect to the database
+\c hrTracker_db
+
+-- Create the department table
 CREATE TABLE department (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL
-    );
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
 
-    CREATE TABLE role (
-       id SERIAL PRIMARY KEY,
-       title VARCHAR(30) UNIQUE NOT NULL,
-       salary DECIMAL NOT NULL,
-       department_id INTEGER NOT NULL
-    );
+-- Create the role table
+CREATE TABLE role (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
 
-    CREATE TABLE employee (
-        id SERIAL PRIMARY KEY
-        first_name VARCHAR(30) NOT NULL,
-        last_name VARCHAR(30) NOT NULL,
-        role_id INTEGER NOT NULL,
-        manager_id INTEGER
-    );
+-- Create the employee table
+CREATE TABLE employee (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER,
+);
+
